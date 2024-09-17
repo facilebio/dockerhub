00a392b090ef068f5487d167a0ff37d628a7599e
Steve Lianoglou <slianoglou@gmail.com>
2024-09-17 11:03:35 -0700

    pak::pkg_install seems to be choking?

--- facilebio_base/base-packages-install.R
+++ facilebio_base/base-packages-install.R
@@ -2,6 +2,6 @@ installed.pkgs <- rownames(installed.packages())
 required.pkgs <- readLines("/tmp/base-packages.txt")
 install.me <- setdiff(required.pkgs, installed.pkgs)
 
-# BiocManager::install(install.me, update = FALSE, ask = FALSE)
-pak::pkg_install(install.me, upgrade = TRUE, ask = FALSE)
+# pak::pkg_install(install.me, upgrade = TRUE, ask = FALSE)
+BiocManager::install(install.me, update = FALSE, ask = FALSE)
 
