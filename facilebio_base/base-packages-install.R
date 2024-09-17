a411de3c926b3f70834abaa937f0a4bc01699134
Steve Lianoglou <slianoglou@gmail.com>
2024-09-17 00:04:41 -0700

    BiocManager::install replaced with pak::pkg_install

--- facilebio_base/base-packages-install.R
+++ facilebio_base/base-packages-install.R
@@ -2,5 +2,6 @@ installed.pkgs <- rownames(installed.packages())
 required.pkgs <- readLines("/tmp/base-packages.txt")
 install.me <- setdiff(required.pkgs, installed.pkgs)
 
-BiocManager::install(install.me, update = FALSE, ask = FALSE)
+# BiocManager::install(install.me, update = FALSE, ask = FALSE)
+pak::pkg_install(install.me, upgrade = TRUE, ask = FALSE)
 
